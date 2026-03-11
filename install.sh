9d2415ef16ed450eda5daadfc2ca70f2f043bdd5
wade <wade@funmobig.com>
2026-03-11 15:50:16 +0800

    chore: 升级版本号至 v1.1.2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 author=wade0317
-is_sh_ver=v1.1.1
+is_sh_ver=v1.1.2
 # github=https://github.com/wade0317/xray
 
 # bash fonts colors
--- xray.sh
+++ xray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 args=$@
-is_sh_ver=v1.1.1
+is_sh_ver=v1.1.2
 
 . /etc/xray/sh/src/init.sh
